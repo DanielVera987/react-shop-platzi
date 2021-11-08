a0a69e37118feff58bec79a91248e1f45ea94a59
Daniel Alberto Vera Angulo <danielveraangulo703@gmail.com>
2021-11-07 20:39:54 -0500

    Mobile menu visibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import '@styles/Header.scss';
 import Menu from '@components/Menu';
 import MyOrder from '@containers/MyOrder';
@@ -8,9 +8,35 @@ import AppContext from '../context/AppContext';
 import iconShopingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(true);
     const [toggle, setToggle] = useState(false);
     const [toggleOrders, setToogleOrders] = useState(false);
     const { state } = useContext(AppContext);
+    const [size, setSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener("resize", handleResize);
+        
+        handleResize();
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (size.width < 640 && menuOpen) {
+            setMenuOpen(false);
+        }else if(size.width > 640 && !menuOpen) {
+            setMenuOpen(true);
+        }
+    }, [size.width]);
 
     const handleToggle = () => {
         setToggle(!toggle);
@@ -18,43 +44,59 @@ const Header = () => {
     
     return (
         <nav>
-            <img src={iconMenu} alt="menu" className="menu" />
-            <div className="navbar-left">
-                <img src={logo} alt="logo" className="nav-logo" />
-                <ul>
-                    <li>
-                        <a href="/">All</a>
-                    </li>
-                    <li>
-                        <a href="/">Clothes</a>
-                    </li>
-                    <li>
-                        <a href="/">Electronics</a>
-                    </li>
-                    <li>
-                        <a href="/">Furnitures</a>
-                    </li>
-                    <li>
-                        <a href="/">Toys</a>
-                    </li>
-                    <li>
-                        <a href="/">Others</a>
-                    </li>
-                </ul>
+            <div className="Navbar__title">
+                <img src={iconMenu} 
+                    className="Navbar__title__icon__mobile" 
+                    alt="menu" 
+                    onClick={() => setMenuOpen(!menuOpen)}
+                />
+                <img src={logo} alt="logo" className="Navbar__title__logo" />
             </div>
-            <div className="navbar-right">
-                <ul>
-                    <li className="navbar-email" onClick={handleToggle}>
-                        platzi@example.com
-                    </li>
-                    <li className="navbar-shopping-cart" 
-                        onClick={() => setToogleOrders(!toggleOrders)}
-                    >
-                        <img src={iconShopingCart} alt="shopping cart" />
-                        {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
-                    </li>
-                </ul>
+
+            {menuOpen
+                ? 
+                    <div className="Navbar__container">
+                        <div className="Navbar__container__title">CATEGORIES</div>
+                        <ul className="Navbar__container__left">
+                            <li>
+                                <a href="/">All</a>
+                            </li>
+                            <li>
+                                <a href="/">Clothes</a>
+                            </li>
+                            <li>
+                                <a href="/">Electronics</a>
+                            </li>
+                            <li>
+                                <a href="/">Furnitures</a>
+                            </li>
+                            <li>
+                                <a href="/">Toys</a>
+                            </li>
+                            <li>
+                                <a href="/">Others</a>
+                            </li>
+                        </ul> 
+
+                        <ul className="Navbar__container__right">
+                            <li className="navbar-email" onClick={handleToggle}>
+                                <a href="#">platzi@example.com</a>
+                            </li>
+                            <li>
+                                <a href="#" className="Navbar__container__sign" >Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
+                : ''
+            }
+
+            <div className="navbar-shopping-cart" 
+                    onClick={() => setToogleOrders(!toggleOrders)}
+                >
+                    <img src={iconShopingCart} alt="shopping cart" />
+                    {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
             </div>
+
             {toggle && <Menu />}
             {toggleOrders && <MyOrder setToogleOrders={setToogleOrders} toggleOrders={toggleOrders}/>}
         </nav>
